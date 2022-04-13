bb50909b999451b7ec97033af3c53aeb9bc496ae
Zmaha106 <mahabubzinia1@gmail.com>
2022-04-12 22:16:19 -0400

    added Michael's changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,16 @@ var formEl = document.querySelector("#bill-form");
 var tasksToDoEl = document.querySelector("#bill-to-calc");
 var billIdCounter = 0;
 var pageContentEl = document.querySelector("#page-content");
+var APIkeyWeather = "0ed751aee02b60e3e3109adbed6b18e3"
+var city;
+
+function getWeather(city) {
+  //  Using saved city name, execute a current condition get request from open weather map api
+          let queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0ed751aee02b60e3e3109adbed6b18e3"
+          axios.get(queryURL)
+          .then(function(response){
+              console.log(response);
+
 
 var billFormHandler = function(event) {
   event.preventDefault();
@@ -22,4 +32,3 @@ var billFormHandler = function(event) {
     name: billNameInput,
     type: peopleAmountInput
   };
-  }
