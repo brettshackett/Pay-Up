37108472d08ddc24ab6cb48f98f9d6c6d9334f69
Jason Yoo <sungminyoo@gmail.com>
2022-04-13 18:53:37 -0400

    started js query

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,8 @@
-var formEl = document.querySelector("#bill-form");
-var tasksToDoEl = document.querySelector("#bill-to-calc");
-var billIdCounter = 0;
-var pageContentEl = document.querySelector("#page-content");
+$(function(){
 
-var billFormHandler = function(event) {
-  event.preventDefault();
-  var billNameInput = document.querySelector("input[name='bill-name'").value;
-  var peopleAmountInput = document.querySelector("input[name='peopleamount']").value;
+$('#add').click(function(){
+  var form = $('.person-bill:last').clone();
+  $(form).insertAfter('.person-bill:last'); 
+});
 
-//   i know we cant use alerts, this is a temporary placeholder
-  if (billNameInput === "" || peopleAmountInput === "") {
-    alert("You need to fill out the task form!");
-    return false;
-  }
-  formEl.reset();
-
-  document.querySelector("input[name='bill-name']").value = "";
-  document.querySelector("input[name='bill-type']").selectedIndex = 0;
-
-  var billDataObj = {
-    name: billNameInput,
-    type: peopleAmountInput
-  };
-  }
+})
