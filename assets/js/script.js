660b31bac9381194c3b462240a4913c6bd9c086b
Jason Yoo <sungminyoo@gmail.com>
2022-04-14 20:32:17 -0400

    add item js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
-$(function(){
+$(document).ready(function() {
 
-$('#add').click(function(){
-  var form = $('.person-bill:last').clone();
-  $(form).insertAfter('.person-bill:last'); 
+  $("#add").click(function() {
+      var
+      $self = $(this),
+          $element_to_clone = $self.prev(),
+          $wrapper_parent_element = $self.parent(),
+          $new_element = $element_to_clone.clone();
+
+      $new_element.find('.del').removeClass('hidden disabled').addClass('enabled');
+
+      $new_element.insertAfter($element_to_clone);
+
+      return false;
+  });
+
+  $("body").on("click", ".del.enabled", function(event) {
+      var $parent = $(this).parent();
+      $parent.remove();
+      return false;
+  });
 });
-
-$('#remove').click(function(){
-  $('.person-bill:last').remove();
-});
-
-})
