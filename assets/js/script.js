589bcdfec9c3229153f264ef289dd5a091c87a71
mmikelic <mikelicmichael@gmail.com>
2022-04-14 20:09:48 -0400

    updates Mike

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,3 +33,51 @@ var billFormHandler = function(event) {
     type: peopleAmountInput
   };
   };
+
+var index_current_location_json = localStorage.getItem("lifestyle-location");
+
+if (!index_current_location_json) {
+    window.location = "./welcome.html";
+}
+
+var index_location = JSON.parse(index_current_location_json);
+
+$("#index-current-location").html(index_location.name + "<br>" + index_location.vicinity);
+
+function LoadContent(path, target) {
+    fetch(path)
+        .then(function(response) {
+            if (response.ok) {
+                response.text()
+                    .then(function(text) {
+                        $(target).html(text);
+                    })
+            }
+    })
+}
+
+LoadContent("./content/contacts.html", "#contacts-content");
+LoadContent("./content/tasks.html", "#tasks-content");
+LoadContent("./content/shoppinglist.html", "#shopping-list-content");
+LoadContent("./content/weather.html", "#weather-content");
+LoadContent("./content/myarea.html", "#my-area-content");
+
+var searchBox;
+var addressInput = document.getElementById("address");
+
+async function initAutocomplete() {
+
+    searchBox = new google.maps.places.SearchBox(addressInput);
+
+    searchBox.addListener("places_changed", () => {
+        const places = searchBox.getPlaces();
+        
+        if (places.length < 1) {
+            return;
+        }
+
+        localStorage.setItem("lifestyle-location", JSON.stringify(places[0]));
+
+        window.location = "./index.html";
+    });
+}
