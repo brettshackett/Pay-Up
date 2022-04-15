b1778b209c6c5aee33178cdf41a76da592655fed
Jason Yoo <sungminyoo@gmail.com>
2022-04-14 23:05:15 -0400

    redid js to see one bill to split

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,36 @@
 $(document).ready(function() {
 
-  $("#add").click(function() {
-      var
-      $self = $(this),
-          $element_to_clone = $self.prev(),
-          $wrapper_parent_element = $self.parent(),
-          $new_element = $element_to_clone.clone();
+  var sliders = document.querySelectorAll("input[type='range']");
+  sliders.forEach(function(slider){
+  slider.addEventListener("input",calculateTip);
+});
 
-      $new_element.find('.del').removeClass('hidden disabled').addClass('enabled');
+var billInput = document.getElementById("bill");
+billInput.addEventListener("change",calculateTip);
 
-      $new_element.insertAfter($element_to_clone);
 
-      return false;
-  });
+function calculateTip(){
+    let bill = parseFloat(billInput.value);
+    let tipPercent = document.getElementById("tip").value;
+    let noOfPeople = document.getElementById("no-of-people").value;
 
-  $("body").on("click", ".del.enabled", function(event) {
-      var $parent = $(this).parent();
-      $parent.remove();
-      return false;
-  });
+    billInput.value = bill.toFixed(2);
+
+    let totalTip = parseFloat((bill * (tipPercent/100)).toFixed(2));
+    let total = parseFloat((bill + totalTip).toFixed(2));
+
+    let tipPerPerson = (totalTip / noOfPeople).toFixed(2);
+    let totalPerPerson = (total / noOfPeople).toFixed(2);
+
+    document.getElementById("tip-amount").textContent = `\$ ${totalTip}`;
+    document.getElementById("total-amount").textContent = `\$ ${total}`;
+    
+    document.getElementById("tip-percent").textContent = `${tipPercent}%`;
+    document.getElementById("split-num").textContent = noOfPeople;
+
+    document.getElementById("tip-per-person").textContent = `\$ ${tipPerPerson}`;
+    document.getElementById("total-per-person").textContent = `\$ ${totalPerPerson}`;
+}
+calculateTip();
+  
 });
